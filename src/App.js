7866aaa24aeeaff1eb99b5c52d9078021daccc3f
d2luu <luubkk57@gmail.com>
2018-06-05 18:01:39 +0900

    Push notification

--- src/App.js
+++ src/App.js
@@ -7,8 +7,40 @@
 import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
 import {SideMenu} from './Router';
+import OneSignal from 'react-native-onesignal';
+
+const YOUR_ONESIGNAL_APPID = "5ceaa120-c010-4fef-bfd4-75246165f4c2";
 
 export default class App extends Component {
+  componentDidMount() {
+    OneSignal.init(YOUR_ONESIGNAL_APPID);
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
   render() {
     return (
       <SideMenu />
--- src/networking/fetchApi.js
+++ src/networking/fetchApi.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
 
 const apiCoinMarketCap = 'https://api.coinmarketcap.com/v1/ticker/?limit=10';
-const apiToInsert = '';
-const apiToEdit = '';
 const apiInsertNewDogLocal = 'http://localhost:3000/insert_new_dog';
 const apiDogListLocal = 'http://localhost:3000/list_all_dog';
 const apiUpdateADogLocal = 'http://localhost:3000/update_a_dog';
@@ -21,40 +18,6 @@ async function getCoinList() {
     return resJson;
   } catch (e) {
     console.log(`Error is: ${e}`);
-  }
-}
-
-async function insertNewElementToServer(params) {
-  try {
-    let res = await fetch(apiToInsert, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(params)
-    });
-    let resJson = await res.json();
-    return resJson
-  } catch (e) {
-    console.error(`Error is: ${e}`)
-  }
-}
-
-async function updateElementToServer(params) {
-  try {
-    let res = await fetch(apiToInsert, {
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(params)
-    });
-    let resJson = await res.json();
-    return resJson
-  } catch (e) {
-    console.error(`Error is: ${e}`)
   }
 }
 
@@ -124,5 +87,3 @@ export {getDogList};
 export {insertNewDogToServer};
 export {updateADog};
 export {deleteADog};
-export {insertNewElementToServer};
-export {updateElementToServer};
